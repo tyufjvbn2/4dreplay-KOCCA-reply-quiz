d29d9bc1f985a6722fca0ca6dd59bc82a43a95de
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-09-03 11:33:30 +0900

    fix: remove unused text

--- src/graphql/root/root.ts
+++ src/graphql/root/root.ts
@@ -1,12 +1,4 @@
-// import Mongoose from "mongoose";
-// import { config } from "../../config/config";
-
-// const db = Mongoose.createConnection("mongodb://localhost:27017/kocca_dev");
 const Replys = require("../../models/collections/reply");
-
-// const Reply = db.model("Reply", replySchema);
-
-// import Report from "models/collections/report";
 
 export const root = {
   test: () => "Query is working well!",

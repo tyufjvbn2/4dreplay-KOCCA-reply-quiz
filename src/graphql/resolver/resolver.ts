dbfdf71c37aaa8366097ef665185f09a8606ee07
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-09-08 11:17:41 +0900

    feat: done basic requirement query

--- src/graphql/resolver/resolver.ts
+++ src/graphql/resolver/resolver.ts
@@ -2,13 +2,47 @@ import mongoose from "mongoose"
 const Reply = require("../../models/collections/reply");
 const Report = require("../../models/collections/report");
 
+enum ReplyState {
+  OPEN = "OPEN",
+  UNCHECKED = "UNCHECKED",
+  CHECKED = "CHECKED",
+  BLOCK = "BLOCK"
+}
+
+enum ReportState {
+  UNCHECKED = "UNCHECKED",
+  CHECKED = "CHECKED",
+  DONE = "DONE"
+}
+
+enum ReportReason {
+  BAD_NICKNAME = "BAD_NICKNAME",
+  VIOLENT_TEXT = "VIOLENT_TEXT",
+  SEXUAL_TEXT = '"SEXUAL_TEXT',
+  AD_TEXT = "AD_TEXT",
+  REPEATED = "REPEATED",
+  OUTLAW = "OUTLAW",
+  ETC = "ETC"
+}
+
 interface Params {
-  vod_id: mongoose.Types.ObjectId,
-  user_id: mongoose.Types.ObjectId,
-  reply_text: string | undefined
+  _id?: mongoose.Types.ObjectId,
+  vod_id?: mongoose.Types.ObjectId,
+  user_id?: mongoose.Types.ObjectId,
+  reply_id?: mongoose.Types.ObjectId,
+  reply_text?: string | undefined,
+  reply_report_state?: ReplyState,
+  admin_check_state?: ReportState,
+  report_reason?: ReportReason,
+  report_text?: string
 }
 
 export const resolver = {
+
+  //--------------//
+  // Query Method //
+  //--------------//
+
   test: () => {
     return "Query is working well!";
   },
@@ -26,11 +60,6 @@ export const resolver = {
     console.log("byVod", byVod);
     return byVod;
   },
-  // countTotalReply: async () => {
-  //   const count = await Reply.count();
-  //   console.log("how many", count);
-  //   return count;
-  // },
   replyByUserId: async (user_id: string) => {
     const byUserId = await Reply.find(user_id)
     console.log("byUserId", byUserId)
@@ -46,9 +75,13 @@ export const resolver = {
     return await Report.find(reply_id)
   },
 
-  createReply: async (obj: Params) => {
-    console.log("params", obj)
-    const { vod_id, user_id, reply_text } = obj
+  //-----------------//
+  // Mutation Method //
+  //-----------------//
+
+  createReply: async (arg: Params) => {
+    console.log("params", arg)
+    const { vod_id, user_id, reply_text } = arg
     const newOne = await Reply.create({
       vod_id: vod_id,
       user_id: user_id,
@@ -59,5 +92,71 @@ export const resolver = {
     })
     console.log("created", newOne)
     return newOne
-  }
+  },
+
+  deleteReply: async (_id: mongoose.Types.ObjectId) => {
+    const target = await Reply.findOneAndDelete(_id)
+    console.log("target", target)
+    return target
+  },
+
+  updateReply: async (arg: Params) => {
+    const { _id, reply_text } = arg
+    console.log("params", _id, reply_text)
+    const update = await Reply.findOneAndUpdate(
+      { _id: _id },
+      {
+        reply_text: reply_text,
+        updated_at: new Date()
+      },
+      { returnOriginal: false })
+    console.log("update", update)
+    return update
+  },
+
+  changeReplyReportState: async (arg: Params) => {
+    const { _id, reply_report_state } = arg
+    const adminChk = await Reply.findOneAndUpdate(
+      { _id: _id },
+      {
+        reply_report_state: reply_report_state,
+        updated_at: new Date()
+      },
+      { returnOriginal: false })
+    console.log("adchk", adminChk)
+    return adminChk
+  },
+
+  createReport: async (arg: Params) => {
+    const { reply_id, user_id, report_reason, report_text } = arg
+    const newReport = await Report.create({
+      reply_id: reply_id,
+      user_id: user_id,
+      report_reason: report_reason,
+      report_text: report_text,
+      admin_check_state: "UNCHECKED",
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+    console.log("new", newReport)
+    return newReport
+  },
+
+
+  checkReport: async (arg: Params) => {
+    const { _id, admin_check_state } = arg
+    const rptChk = await Report.findOneAndUpdate(
+      { _id: _id },
+      {
+        admin_check_state: admin_check_state,
+        updated_at: new Date()
+      },
+      { returnOriginal: false }
+    )
+    console.log("chk", rptChk)
+    return rptChk
+  },
+
+
+
 };
--- src/graphql/schema/schema.ts
+++ src/graphql/schema/schema.ts
@@ -64,5 +64,10 @@ export const schema = buildSchema(`
     
     type Mutation {
       createReply(vod_id: objectId, user_id: objectId, reply_text: String) : Reply
+      deleteReply(_id: objectId): Reply
+      updateReply(_id: objectId, reply_text: String): Reply
+      changeReplyReportState(_id: objectId, reply_report_state: ReplyState): Reply
+      createReport(reply_id: objectId, user_id: objectId, report_reason: ReportReason, report_text: String): Report
+      checkReport(_id: objectId, admin_check_state: ReportState) : Report
     }`
 );

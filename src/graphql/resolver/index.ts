892558ef746d427150bba5f94f1d98accbd9e559
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-10-08 16:58:36 +0900

    feat: delete policy part from this repo

--- src/graphql/resolver/index.ts
+++ src/graphql/resolver/index.ts
@@ -2,12 +2,10 @@ import { replyResolver } from "./resolvers/replyResolver";
 import { quizResolver } from "./resolvers/quizResolver";
 import { noticeResolver } from "./resolvers/noticeResolver";
 import { quizUserResolver } from "./resolvers/quizUserResolver";
-import { policyResolver } from "./resolvers/policyResolver";
 
 export const resolver = {
 	...replyResolver,
 	...quizResolver,
 	...noticeResolver,
 	...quizUserResolver,
-	...policyResolver,
 };

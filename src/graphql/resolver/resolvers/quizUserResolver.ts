489478ea70abcb31af46c63c1a2c7b544886914b
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-10-13 20:06:59 +0900

    feat: quiz user data model add

--- src/graphql/resolver/resolvers/quizUserResolver.ts
+++ src/graphql/resolver/resolvers/quizUserResolver.ts
@@ -1,7 +1,8 @@
 // import mongoose from "mongoose";
-const quizUser = require("../../../models/collections/quiz_user");
+const QuizUser = require("../../../models/collections/quiz_user");
+const QuizUserData = require("../../../models/collections/quiz_user_data");
 
-// import { noticeParams } from "../interfaces/noticeInterface";
+import { QuizUserParams } from "../interfaces/quizUserInterface";
 
 //퀴즈의 항목을 유저가 선택하면 그 값을 데이터에 찍는 부분
 
@@ -15,7 +16,7 @@ export const quizUserResolver = {
 	},
 
 	quizUserList: async () => {
-		const target = await quizUser.distinct("user_id"); // 지정된 키값을 중복제거하여 가져옴
+		const target = await QuizUser.distinct("user_id"); // 지정된 키값을 중복제거하여 가져옴
 		console.log("target", target);
 		return target;
 	},
@@ -23,4 +24,31 @@ export const quizUserResolver = {
 	//-----------------//
 	// Mutation Method //
 	//-----------------//
+
+	selectQuizOption: async (arg: QuizUserParams) => {
+		const { quiz_id, user_id, option } = arg;
+		const newData = await QuizUser.findOneAndUpdate(
+			{ user_id: user_id },
+			{
+				quiz_id: quiz_id,
+				user_id: user_id,
+				option: option,
+			},
+			{ upsert: true, returnOriginal: false }
+		);
+		console.log("user select option", newData);
+
+		return;
+	},
+
+	addQuizParticipantData: async (arg: QuizUserParams) => {
+		const { quiz_id, user_id, name, phone_number } = arg;
+		const newData = await QuizUserData.create({
+			quiz_id: quiz_id,
+			user_id: user_id,
+			name: name,
+			phone_number: phone_number,
+		});
+		console.log("new personal data", newData);
+	},
 };
